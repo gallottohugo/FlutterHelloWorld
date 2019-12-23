f2e2d1dd934c1fc059b3351bab39a21e3b84312d
gallottohugo <gallottohugo@gmail.com>
2019-12-23 12:35:05 -0300

    Change HomePage to SumPage

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hello_world/src/pages/home_page.dart';
+import 'package:flutter_hello_world/src/pages/sum_page.dart';
+//import 'package:flutter_hello_world/src/pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   	@override
   	Widget build(context) {
     	return MaterialApp(
       		home: Center(
-        		child: HomePage(),
+				//child: HomePage(),
+        		child: SumPage(),
       		)
     	);
   	}
